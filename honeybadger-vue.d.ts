e79104d0f313c29ddefed40da0f3a95a397b1c76
Muzafar Umarov <umarov@gmail.com>
2021-01-25 23:09:51 -0500

    fix: type definitions to include install function

--- honeybadger-vue.d.ts
+++ honeybadger-vue.d.ts
@@ -1,14 +1,24 @@
-import Vue from 'vue';
-import Honeybadger from 'honeybadger-js';
+// This is required for Vue 3 createApp support
+import { App } from '@vue/runtime-core'
+import _Vue from 'vue'
+import Honeybadger from '@honeybadger-io/js';
 
 interface HoneybadgerVue {
-    notify(...args: any[]): any;
-    setContext<T extends object>(context: T): Honeybadger;
-    resetContext(): Honeybadger;
+  notify(...args: any[]): any;
+  setContext<T extends object>(context: T): typeof Honeybadger;
+  resetContext(): typeof Honeybadger;
+}
+
+declare module '@honeybadger-io/vue' {
+  const HoneybadgerVue: {
+    install(app: App | typeof _Vue, options?: any): void
+  }
+
+  export default HoneybadgerVue;
 }
 
 declare module 'vue/types/vue' {
-    interface Vue {
-        $honeybadger: HoneybadgerVue;
-    }
+  interface Vue {
+    $honeybadger: HoneybadgerVue;
+  }
 }
