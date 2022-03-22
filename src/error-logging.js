888b0079504def2099cced42e368421ca901d2f8
Pangratios Cosma <pagcosma@gmail.com>
2022-03-22 22:43:01 +0200

    log errors in vue3, same was as in vue2

--- src/error-logging.js
+++ src/error-logging.js
@@ -1,11 +1,11 @@
 import { generateComponentTrace } from './vue-debug'
 
-export function logError (Vue, error, vm, info) {
+export function logError (app, error, vm, info) {
   const message = `Error in ${info}: "${error && error.toString()}"`
 
   const trace = vm ? generateComponentTrace(vm) : ''
-  if (Vue.config.warnHandler) {
-    Vue.config.warnHandler.call(null, message, vm, trace)
+  if (app.config.warnHandler) {
+    app.config.warnHandler.call(null, message, vm, trace)
   } else {
     console.error(`[Vue warn]: ${message}${trace}`)
   }
