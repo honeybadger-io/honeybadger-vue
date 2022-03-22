51ed2c3aa60b6af81f829d3da4c1eeecceac9d76
Pangratios Cosma <pagcosma@gmail.com>
2022-03-22 21:14:47 +0200

    #865 (Vue.js v2) Log errors to console  (#880)
    * log errors in console in development mode
    
    * changelog
    
    * refactor logError to separate file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Honeybadger from '@honeybadger-io/js'
+import { logError } from './error-logging'
 
 const HoneybadgerVue = {
   install (Vue, options) {
@@ -10,10 +11,14 @@ const HoneybadgerVue = {
     Vue.prototype.$honeybadger = Vue.$honeybadger
     const chainedErrorHandler = Vue.config.errorHandler
     const extractContext = function (vm) {
-      var options = typeof vm === 'function' && vm.cid != null ? vm.options : vm._isVue ? vm.$options ||
-        vm.constructor.options : vm || {}
-      var name = options.name || options._componentTag
-      var file = options.__file
+      const options = typeof vm === 'function' && vm.cid != null
+        ? vm.options
+        : vm._isVue
+          ? vm.$options ||
+        vm.constructor.options
+          : vm || {}
+      const name = options.name || options._componentTag
+      const file = options.__file
       return {
         isRoot: vm.$root === vm,
         name: name,
@@ -22,10 +27,24 @@ const HoneybadgerVue = {
         file: file
       }
     }
+    const shouldLogError = () => {
+      if (Vue.config.warnHandler) {
+        return true
+      }
+
+      const hasConsole = typeof console !== 'undefined'
+      const isDebug = Vue.config.debug || process.env.NODE_ENV !== 'production'
+      return hasConsole && isDebug
+    }
+
     Vue.config.errorHandler = (error, vm, info) => {
       honeybadger.notify(error, { context: { vm: extractContext(vm), info: info } })
       if (typeof chainedErrorHandler === 'function') {
         chainedErrorHandler.call(this.Vue, error, vm, info)
+      }
+
+      if (shouldLogError()) {
+        logError(Vue, error, vm, info)
       }
     }
   }
